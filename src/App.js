4ad8ec06e1bf0bc10095f3e9948cdeaf8f7b0346
abed-muntasir <muntasir.abed01@gmail.com>
2018-09-15 12:07:39 +0100

    add api request and add a loader

--- src/App.js
+++ src/App.js
@@ -10,26 +10,41 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+        console.log('1');
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => {
+                this.setState({ robots: users });
+            })
+        
     }
 
     onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value})
+        this.setState({ searchfield: event.target.value })
     }
 
     render() {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <Cardlist robots={filteredRobots} />
-            </div>
-        );
+        if(this.state.robots.length === 0){
+            return <h1>Loading</h1>
+        } else{
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Cardlist robots={filteredRobots} />
+                </div>
+            );
+        }
+        
     }
 }
 
